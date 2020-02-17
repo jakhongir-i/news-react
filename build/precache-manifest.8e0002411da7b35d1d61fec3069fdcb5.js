613c6ad05156579fcdf117dd5c934251680059d3
jakhongir-i <jahangirinatov@gmail.com>
2020-02-17 15:17:29 +0500

    build

---
+++ build/precache-manifest.8e0002411da7b35d1d61fec3069fdcb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981466bea49c39f7cfde0748be32661d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efacec76a4c90769223e",
+    "url": "/static/css/2.6bf38e3a.chunk.css"
+  },
+  {
+    "revision": "4fdeee748ef8107142f4",
+    "url": "/static/css/main.7d0ae183.chunk.css"
+  },
+  {
+    "revision": "efacec76a4c90769223e",
+    "url": "/static/js/2.4f1f94b0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4f1f94b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdeee748ef8107142f4",
+    "url": "/static/js/main.0808444a.chunk.js"
+  },
+  {
+    "revision": "80a0c74fbf5104e264a2",
+    "url": "/static/js/runtime-main.acc6da19.js"
+  },
+  {
+    "revision": "ecd88ce4970eb61bf188fec6ea2436c6",
+    "url": "/static/media/spinner.ecd88ce4.svg"
+  }
+]);
